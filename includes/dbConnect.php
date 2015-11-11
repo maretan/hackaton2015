625a52ba42838e9fb22d96946ed15fdec33ebf17
slennie12 <bout0036@hz.nl>
2015-11-11 17:00:36 +0100

    add appartment form ready :D

---
+++ includes/dbConnect.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slennie12
+ * Date: 11/11/15
+ * Time: 4:08 PM
+ */
+$url = "localhost";
+$user = "root";
+$password = "";
+$database = "zeehondje";
+$mysqli = new mysqli($url, $user, $password, $database);
+
+if($mysqli->connect_errno > 0){
+    die('Unable to connect to database [' . $mysqli->connect_error . ']');
+}
+
+?>
